bc7e6115bc79ec5998c2c7d646df348228ca74c8
jacobwilson81503 <160570610+jacobwilson81503@users.noreply.github.com>
2024-09-16 16:23:35 -0400

    Add files via upload

---
+++ Wilson Cyvbergaurd good web.js
@@ -0,0 +1,5 @@
+// Function to switch from the entry screen to the home screen
+function showHomeScreen() {
+    document.getElementById('entry-screen').style.display = 'none';
+    document.getElementById('home-screen').style.display = 'block';
+}
